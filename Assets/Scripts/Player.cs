030697e2b6771bb6e1b7c17443d1fcd02693199c
Pedro Esteves <up201705160@fe.up.pt>
2019-01-25 21:59:58 +0000

    Fixed some things in the Player Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,82 +4,56 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private uint jumpPosition;
     private bool startJump;
-    private bool up;
+    private Rigidbody2D rigidBody;
+    private float jumpInitialPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        jumpPosition = 0;
         startJump = false;
+        rigidBody = this.GetComponent<Rigidbody2D>();
+        jumpInitialPosition = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (transform.position.y <= jumpInitialPosition)
+        {
+            startJump = false;
+        }
+
         if (Input.GetKeyDown(KeyCode.W)) //Saltar
         {
             if (!startJump)
             {
                 startJump = true;
+                jumpInitialPosition = transform.position.y;
+                rigidBody.AddForce(Vector2.up * 300f);
             }
         }
-        else if (Input.GetKeyDown(KeyCode.S)) //Baixar
+
+        if (Input.GetKeyDown(KeyCode.S)) //Baixar
         {
-            if (startJump)
-            {
-                up = false;
-            }
+            rigidBody.AddForce(-Vector2.up * 300f);
         }
-        else if (Input.GetKey(KeyCode.D)) //Andar para a direita
+
+        if (Input.GetKey(KeyCode.D)) //Andar para a direita
         {
-            transform.Translate(.5f, 0, 0);
+            transform.Translate(.1f, 0, 0);
         }
-        else if (Input.GetKey(KeyCode.A)) //Andar para a esquerda
+
+        if (Input.GetKey(KeyCode.A)) //Andar para a esquerda
         {
-            transform.Translate(- .5f, 0, 0);
+            transform.Translate(-.1f, 0, 0);
         }
-        else if (Input.GetKeyDown(KeyCode.LeftControl)) //Agarrar
+
+        if (Input.GetKeyDown(KeyCode.LeftControl)) //Agarrar
         {
 
-        }
-        /*else if (Input.GetKeyDown(")
-        {
-
-        }*/
-
-        if (startJump == true)
-        {
-            jump();
         }
     }
 
-    void jump()
-    {
-        if (jumpPosition < 100)
-        {
-            if (up)
-            {
-                transform.Translate(0, .5f, 0);
-            }
-            else
-            {
-                if (jumpPosition == 0)
-                {
-                    startJump = false;
-                }
-                else
-                {
-                    transform.Translate(0, -.5f, 0);
-                }
-
-            }
-        }
-        else if (jumpPosition == 100)
-        {
-            up = false;
-        }
-
-    }
 }
