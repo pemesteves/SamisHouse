b33aea9cf9deae88aca51e73bba4828782dd50dc
Márcia Teixeira <marcia309@hotmail.com>
2019-01-26 14:20:12 +0000

    Added fox animation

---
+++ Assets/Scripts/FoxMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fox_movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*transform.Translate(.5f, 0, 0);
+
+        Vector3 diff = transform.Position();*/
+    }
+}
