776061b8c9ad843ab1b349ed51d0787541065403
Mário Gil <mg_mesquita@hotmail.com>
2019-01-26 23:20:39 +0000

    ATTACKUUU EAGRUUU

---
+++ Assets/Scripts/EagleBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleBehaviour : MonoBehaviour
+{
+    private Animator anim;
+    private GameObject player;
+    private float move_dir = 1;
+
+    public float move_speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        player = GameObject.FindObjectOfType<PlayerMovement>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 player_dir = Vector3.Normalize(player.transform.position - transform.position);
+        player_dir.z = 0f;
+
+        if (player_dir.x * move_dir < 0)
+        {
+            anim.SetTrigger("change_dir");
+            move_dir *= -1;
+        }
+
+        transform.position += player_dir * move_speed;
+    }
+}
---
+++ Assets/Scripts/SpawnEagle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEagle : MonoBehaviour
+{
+    private bool spawned = false;
+    private Vector3 nest_position;
+
+    public GameObject eagle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nest_position = GameObject.Find("key").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!spawned)
+        {
+            if (!GameObject.Find("key"))
+            {
+                Debug.Log("Key is gone");
+                spawned = true;
+
+                Invoke("spawn_eagle", 1);
+            }
+        }       
+    }
+
+    private void spawn_eagle()
+    {
+        Instantiate(eagle, nest_position, Quaternion.identity);
+    }
+}
