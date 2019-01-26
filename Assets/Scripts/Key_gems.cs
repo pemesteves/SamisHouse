09ae8a130c0ad1624bffa1a397428604d6784d31
Márcia Teixeira <marcia309@hotmail.com>
2019-01-26 19:35:39 +0000

    Added level 4

---
+++ Assets/Scripts/Key_gems.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key_gems : MonoBehaviour
+{
+    public GameObject key;
+    private bool has_key = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool all_on = true;
+
+        foreach(Transform child in transform)
+        {
+            if (!child.gameObject.GetComponent<Gems>().getIsOn())
+            {
+                all_on = false;
+            }
+        }
+
+        if (all_on && !has_key)
+        {
+            Instantiate(key, new Vector3(0.29f, 1.8f, 0f), Quaternion.identity);
+            has_key = true;
+        }
+    }
+}
