54fe36658821c6b65e36f46411698dc92c244ae4
Pedro Esteves <up201705160@fe.up.pt>
2019-01-26 12:06:00 +0000

    Fixed errors

--- Assets/Scripts/Camara.cs
+++ Assets/Scripts/Camara.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Camara : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
