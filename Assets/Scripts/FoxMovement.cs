0b14c8373016f103e6f225a2a79133ceeb468dca
Márcia Teixeira <marcia309@hotmail.com>
2019-01-26 15:10:56 +0000

    Added fox animation lvl 3

--- Assets/Scripts/FoxMovement.cs
+++ Assets/Scripts/FoxMovement.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fox_movement : MonoBehaviour
+public class FoxMovement : MonoBehaviour
 {
+    private float fox_move=0f;
+    private float dir = 1f;
+    private Animator anim;
+
+
+
+    public float max_mov = 10f;
+    public float speed = 0.2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*transform.Translate(.5f, 0, 0);
+       
+        transform.Translate(speed * dir, 0, 0);
+        fox_move += speed;
+        if (fox_move > max_mov)
+        {
+            fox_move = 0;
+            dir *= -1f;
+            anim.SetTrigger("change_dir");
 
-        Vector3 diff = transform.Position();*/
+        }
+
+        //Vector3 diff = transform.Position();
     }
 }
