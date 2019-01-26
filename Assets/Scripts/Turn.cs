d967afd92feb1d33f267594788f0b730e6e4b927
Nuno Cardoso <ns.cardoso@hotmail.com>
2019-01-26 20:37:50 +0000

    Added crank system

---
+++ Assets/Scripts/Turn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour
+{
+    public Sprite[] crank_img;
+    private bool is_down = true;
+    private SpriteRenderer img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.transform.tag == "Player" && Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (is_down)
+            {
+                is_down = false;
+                img.sprite = crank_img[1];
+            }
+            else
+            {
+                is_down = true;
+                img.sprite = crank_img[0];
+            }
+        }
+    }
+}
