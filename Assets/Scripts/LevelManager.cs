ec31b6cf7f1f0615428d284665e8babb791b29b3
Mário Gil <mg_mesquita@hotmail.com>
2019-01-26 14:59:49 +0000

    Level manager and door control

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        Application.LoadLevel("Level 1");
+    }
+
+    public void loadLevel(string level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
