26957916a3cd594c67cb4be0637efb1fa7ba41fd
Pedro Esteves <up201705160@fe.up.pt>
2019-01-27 03:04:24 +0000

    Hide key in lvl 5

---
+++ Assets/Frog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int random_number = Random.Range(0, 2);
+        anim
+
+        if(random_number == 0)
+        {
+            anim.SetTrigger("jump");
+        }
+    }
+}
