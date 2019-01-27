1e128106bfd2fd13457486626fa49c75094e493e
Mário Gil <mg_mesquita@hotmail.com>
2019-01-27 14:42:35 +0000

    Final level fade animation on clicking cat

---
+++ Assets/CatEnd.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatEnd : MonoBehaviour
+{
+
+    private Animator anim;
+
+    private bool ended = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GameObject.Find("UI").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D coll)
+    {
+        if (coll.gameObject.transform.tag == "Player" && Input.GetKeyDown(KeyCode.LeftControl) && !ended)
+        {
+            anim.SetTrigger("fade_animation");
+            ended = true;
+        } 
+    }
+}
