242648184676447cf1fb5e8a3899bb93e997f544
Mário Gil <mg_mesquita@hotmail.com>
2019-01-27 08:31:57 +0000

    Textbox implementation

---
+++ Assets/Scripts/TextBox.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBox : MonoBehaviour
+{
+    private PlayerMovement player;
+    public TextBox_Text current;
+
+
+    void Awake()
+    {
+        player = GameObject.FindObjectOfType<PlayerMovement>();
+        player.enabled = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (!current.get_next_text())
+            {
+                player.enabled = true;
+                Destroy(gameObject);
+            }
+            else
+            {
+                current = GameObject.FindObjectOfType<TextBox_Text>();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TextBox_Text.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBox_Text : MonoBehaviour
+{
+
+    public GameObject next;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool get_next_text()
+    {
+        if (!next) return false;
+
+        next.SetActive(true);
+        gameObject.SetActive(false);
+        return true;
+    }
+}
