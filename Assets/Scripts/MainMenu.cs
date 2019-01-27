f35ab34e625b7dc0e12c5f4623937c12d21c26d5
Pastilhas <up201704982@fe.up.pt>
2019-01-27 15:50:34 +0000

    Super Secrete Main Menu Setting

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Update()
+    {
+        if (Input.GetKey("1"))
+        {
+            SceneManager.LoadScene("Level 1");
+        }
+
+        if (Input.GetKey("2"))
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+
+        if (Input.GetKey("3"))
+        {
+            SceneManager.LoadScene("Level 3");
+        }
+
+        if (Input.GetKey("4"))
+        {
+            SceneManager.LoadScene("Level 4");
+        }
+
+        if (Input.GetKey("5"))
+        {
+            SceneManager.LoadScene("Level 5");
+        }
+
+        if (Input.GetKey("6"))
+        {
+            SceneManager.LoadScene("Level 6");
+        }
+    }
 
     public void PlayGame()
     {
