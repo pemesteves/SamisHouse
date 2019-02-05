7205e5477b7097c2360a327a9340f20812d38cb9
Pedro Esteves <up201705160@fe.up.pt>
2019-02-05 10:25:52 +0000

    Added easter egg

---
+++ Assets/Scripts/crlh_fds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class crlh_fds : MonoBehaviour
+{
+    public AudioSource crlhfds;
+    public Button samis_face;
+
+    private void Start()
+    {
+        samis_face.onClick.AddListener(TaskOnClick); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick()
+    {
+        crlhfds.Play();
+    }
+}
