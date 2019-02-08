87e1e29862e56df4a3badf472f2d808d474cc262
Pedro Esteves <up201705160@fe.up.pt>
2019-02-08 14:59:32 +0000

    Added a chest

---
+++ Assets/chest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chest : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if((Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl)) && collision.gameObject.transform.tag == "Player")
+        {
+            anim.SetTrigger("Open");
+        }
+    }
+}
